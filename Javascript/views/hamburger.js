6860932cc1779fba55734f874c5c02811e44839c
piggelini <sigridwidfeldt@gmail.com>
2022-02-18 14:34:59 +0100

    added some js for hamburgermenu

--- Javascript/views/hamburger.js
+++ Javascript/views/hamburger.js
@@ -1,1 +1,25 @@
-console.log("hej");
+const hamburger = document.getElementById("hamburgerMenuIcon");
+const body = document.querySelector("body");
+
+
+hamburger.addEventListener("click", (e) => {
+
+    const hamburgerMenu = `
+<nav id="ham-nav">
+<div id="ham-top">  <img id="ham-close" src="/webshop-projekt/pictures/Icons/krysset.png"
+alt="close hamburger menu " class="headerIcons"></div>
+<div id="menu-items">
+    <a href="/webshop-projekt/HTML/homepage.html" class="noLinkStyle">Home</a>
+    <a href="/webshop-projekt/HTML/productlist.html" class="noLinkStyle">Shop</a>
+    <a href="#" target="_blank" class="noLinkStyle">Sign in</a>
+    </a>
+</div>
+</nav>`;
+
+    body.innerHTML += hamburgerMenu;
+
+
+
+
+});
+
