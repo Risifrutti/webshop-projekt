ae23483548e53d5577333de5a870bd29ebfa69e6
piggelini <sigridwidfeldt@gmail.com>
2022-02-18 15:22:57 +0100

    js changed and added to hamburger

--- Javascript/views/hamburger.js
+++ Javascript/views/hamburger.js
@@ -1,25 +1,13 @@
-const hamburger = document.getElementById("hamburgerMenuIcon");
-const body = document.querySelector("body");
+const hamburgerIcon = document.getElementById("hamburgerMenuIcon");
+const closeHamIcon = document.getElementById("ham-close");
+const hamburgerMenu = document.getElementById("hamburger-nav");
 
-
-hamburger.addEventListener("click", (e) => {
-
-    const hamburgerMenu = `
-<nav id="ham-nav">
-<div id="ham-top">  <img id="ham-close" src="/webshop-projekt/pictures/Icons/krysset.png"
-alt="close hamburger menu " class="headerIcons"></div>
-<div id="menu-items">
-    <a href="/webshop-projekt/HTML/homepage.html" class="noLinkStyle">Home</a>
-    <a href="/webshop-projekt/HTML/productlist.html" class="noLinkStyle">Shop</a>
-    <a href="#" target="_blank" class="noLinkStyle">Sign in</a>
-    </a>
-</div>
-</nav>`;
-
-    body.innerHTML += hamburgerMenu;
-
-
-
-
+hamburgerIcon.addEventListener("click", (e) => {
+    hamburgerMenu.classList.remove("hide-ham");
 });
+
+closeHamIcon.addEventListener("click", (e) => {
+    hamburgerMenu.classList.add("hide-ham");
+});
+
 
