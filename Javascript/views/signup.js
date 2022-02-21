d96b3dfc8080569614d5e9aebc31d75eddbaea0d
rimante _ <storulike@gmail.com>
2022-02-21 17:56:45 +0100

    sign in popup funkar

--- Javascript/views/signup.js
+++ Javascript/views/signup.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById('button').addEventListener("click", function() {
 	document.querySelector('.bg-modal').style.display = "flex";
 });
@@ -5,3 +6,14 @@ document.getElementById('button').addEventListener("click", function() {
 document.querySelector('.close').addEventListener("click", function() {
 	document.querySelector('.bg-modal').style.display = "none";
 });
+*/
+
+//New popups coden, to show and hide
+const closePopupButton = document.querySelector(".closePopup"); //Hämtar close knappen
+const becomeMemberButton = document.querySelector(".becomeMemberButton"); //Hämtar become a member button
+const signInPopup = document.querySelector(".background-modal"); //Hämtar popup form
+
+closePopupButton.addEventListener("click", () => {
+	console.log("clicked");
+	signInPopup.style.display = "none";
+})
