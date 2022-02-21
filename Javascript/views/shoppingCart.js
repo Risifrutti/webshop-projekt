0fe387e88dc418b4a26653c06e61f5eff694c6c2
jennieisa <jennieisakssoon97@gmail.com>
2022-02-21 16:00:25 +0100

    Made the input field disabled when clicking proceed to checkout btn

--- Javascript/views/shoppingCart.js
+++ Javascript/views/shoppingCart.js
@@ -1,12 +1,24 @@
 //Dölja och visa Billing and shipping 
 const proceedToCheckoutButton = document.querySelector(".proceedToCheckoutButton"); //Hämtar proceed knappen
 const billingAndShippingBox = document.querySelector(".billingAndShippingBox"); //Hämtar billing&Shipping boxen
-const inputQuantityAndDelete = document.querySelectorAll(".inputQuantityAndDeleteButtonWrapper"); //Hämtar input antal och delete knapp
+let inputQuantityInCart = document.querySelectorAll(".inputQuantityInCart"); //Hämtar input antal
+const deleteProdInCart = document.querySelectorAll(".deleteProdIconInCart"); //Hämtar krysset i varukorgen som tar bort produkt
+
 
 //Lägger till event när man klickar på knappen. Visar billing&Shipping box
 proceedToCheckoutButton.addEventListener("click", () => {
-    billingAndShippingBox.style.display = "flex";
-    proceedToCheckoutButton.style.display = "none";
+
+    billingAndShippingBox.style.display = "flex"; //Visar billing and shipping  
+    proceedToCheckoutButton.style.display = "none"; //Döljer oroceed to checkout knapp
+
+    inputQuantityInCart.forEach(element => { //Gör alla input fältet disabled
+
+        element.disabled = true;
+    })
+
+    deleteProdInCart.forEach(element => { //Gömmer alla delete knappar 
+        element.style.display = "none";
+    })
 
 })
 
