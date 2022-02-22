7f3f742f0a595223168aebec7a9c6577496bfe95
piggelini <sigridwidfeldt@gmail.com>
2022-02-22 19:47:42 +0100

    searchfilter eventlistener added in productlist.js

--- Javascript/utilities.js
+++ Javascript/utilities.js
@@ -1,26 +1,5 @@
-const searchButton = document.getElementById("searchbutton");
-
-searchButton.addEventListener("click", (e), async function () {
-    e.preventDefault();
-
-    const searchField = document.getElementById("searchfield");
-    const searchInput = searchField.value.toUpperCase();
-
-    const data = await fetchFile("/webshop-projekt/Javascript/data/products.json");
-
-    await showProducts();
-    const product = document.getElementById("product-article");
-
-    data.products.forEach(element => {
-        if (!element.name.toUpperCase().includes(searchInput)) {
-            console.log("MUPP");
-        }
-
-    });
 
 
-
-});
 //Funktion som hämtar json fil och returnerar dess data 
 async function fetchFile(file) {
     const response = await fetch(file);
--- Javascript/views/productlist.js
+++ Javascript/views/productlist.js
@@ -8,7 +8,7 @@ async function showProducts() {
     data.products.forEach((element, index) => {
 
         const product =
-            `<article class="product-article" id="product-article">
+            `<article class="product-article">
                 <section class="product-image">
                 <a href="/webshop-projekt/HTML/productsite.html" class="noLinkStyle">
                 <img id="productimage" src=${element.prodImageURL}
@@ -48,10 +48,10 @@ async function saveToCartFromProdList() {
 
             i = e.target.id;
             let prodstring = "";
-            
+
             if (data.products[i]) {
 
-                prodString =+ JSON.stringify(data.products[i]);
+                prodString = + JSON.stringify(data.products[i]);
 
                 localStorage.setItem(`product`, prodString);
             }
@@ -59,4 +59,40 @@ async function saveToCartFromProdList() {
     })
 }
 
-showProducts().then(saveToCartFromProdList()); 
+showProducts().then(saveToCartFromProdList());
+
+
+//--Sökfunktionen--
+const searchButton = document.getElementById("searchbutton");
+
+searchButton.addEventListener("click", async function (e) {
+    e.preventDefault();
+
+    const searchField = document.getElementById("searchfield");
+    const searchInput = searchField.value.toUpperCase();
+    const productList = document.querySelectorAll(".product-article");
+
+    const data = await fetchFile("/webshop-projekt/Javascript/data/products.json");
+
+    //Tar bort hideproduct-klassen från föregående sökningar
+    productList.forEach(element => {
+        if (element.classList.contains("hideProduct")) {
+            element.classList.remove("hideProduct");
+        }
+    });
+
+    //Jämför det användaren sökt på med produktens namn och beskrivning 
+    //och lägger till klassen "hideProduct" på de produkter som inte matchar.
+    data.products.forEach(element => {
+        if (!element.name.toUpperCase().includes(searchInput) && !element.prodDescription.toUpperCase().includes(searchInput)) {
+            productList.forEach(product => {
+                const productName = product.childNodes[3].childNodes[1].childNodes[1];
+                if (element.name.toUpperCase().includes(productName.innerText.toUpperCase())) {
+                    product.classList.add("hideProduct");
+                    console.log(product);
+                }
+            });
+        }
+    });
+});
+
