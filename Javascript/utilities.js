997d63036b75b97f4f44a128911236254c70eb26
piggelini <sigridwidfeldt@gmail.com>
2022-02-22 14:20:10 +0100

    working on searchfilter

--- Javascript/utilities.js
+++ Javascript/utilities.js
@@ -3,12 +3,10 @@ const searchButton = document.getElementById("searchbutton");
 searchButton.addEventListener("click", (e), async function () {
     e.preventDefault();
 
-    const product = document.getElementById("product-article");
     const searchField = document.getElementById("searchfield");
     const searchInput = searchField.value.toUpperCase();
 
-    const response = await fetch("/webshop-projekt/Javascript/data/products.json");
-    const data = await response.json();
+    const data = await fetchFile("/webshop-projekt/Javascript/data/products.json");
 
     await showProducts();
     const product = document.getElementById("product-article");
