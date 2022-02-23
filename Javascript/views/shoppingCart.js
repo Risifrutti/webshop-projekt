31390c3c0bbb8b28336b97e70473eeb466dfd005
victoriakristina <victoriaercin@gmail.com>
2022-02-23 15:10:17 +0100

    Validation kod till shopping cart

--- Javascript/views/shoppingCart.js
+++ Javascript/views/shoppingCart.js
@@ -1,25 +1,39 @@
 //Dölja och visa Billing and shipping 
 const proceedToCheckoutButton = document.querySelector(".proceedToCheckoutButton"); //Hämtar proceed knappen
 const billingAndShippingBox = document.querySelector(".billingAndShippingBox"); //Hämtar billing&Shipping boxen
-let inputQuantityInCart = document.querySelectorAll(".inputQuantityInCart"); //Hämtar input antal
-const deleteProdInCart = document.querySelectorAll(".deleteProdIconInCart"); //Hämtar krysset i varukorgen som tar bort produkt
-
+const inputQuantityAndDelete = document.querySelectorAll(".inputQuantityAndDeleteButtonWrapper"); //Hämtar input antal och delete knapp
 
 //Lägger till event när man klickar på knappen. Visar billing&Shipping box
 proceedToCheckoutButton.addEventListener("click", () => {
-
-    billingAndShippingBox.style.display = "flex"; //Visar billing and shipping  
-    proceedToCheckoutButton.style.display = "none"; //Döljer oroceed to checkout knapp
-
-    inputQuantityInCart.forEach(element => { //Gör alla input fältet disabled
-
-        element.disabled = true;
-    })
-
-    deleteProdInCart.forEach(element => { //Gömmer alla delete knappar 
-        element.style.display = "none";
-    })
+    billingAndShippingBox.style.display = "flex";
+    proceedToCheckoutButton.style.display = "none";
 
 })
 
+const billingandshippingform = document.querySelector(".billingAndShippingForm");
+const errorMessages = document.querySelectorAll(".error");
+
+
+function hideError(){
+    errorMessages.forEach((e) =>{
+        e.style.display = "none";
+    })
+}
+
+billingandshippingform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    hideError();
+    
+    const inputsRegForm = billingandshippingform.querySelectorAll(".billingAndShippingInput");
+    inputsRegForm.forEach((input) => {
+      if(!input.checkValidity()){ //Kollar if inputs are fild or not
+        const errorMsg = document.getElementById(input.id + 'error');
+        errorMsg.style.display = "block";
+      }
+    });
+    
+  });
+
+
+hideError();
 
